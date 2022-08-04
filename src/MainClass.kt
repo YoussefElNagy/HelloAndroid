a819eca988141f461a4b879c4685a6ab6106e5ec
YoussefElNagy <youssef.nagy2002@gmail.com>
2022-08-04 22:07:53 +0200

    2nd Version

--- src/MainClass.kt
+++ src/MainClass.kt
@@ -1,8 +1,8 @@
  fun main() {
-     var x= 6
-     var y= 8
+     var x= 12
+     var y= 9
      var sum=x+y
      println("\n\n\n\n")
      println(sum)
-  //first version
+  //second version
     }
